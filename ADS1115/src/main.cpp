486d0b1cddf7d8cc1c300c6fcce20c6db83248ba
axel <axel.gantou@carnus.fr>
2025-11-24 10:38:52 +0100

    Implement DC motor control based on distance
    Added motor control for DC motor based on distance measurement.

--- ADS1115/src/main.cpp
+++ ADS1115/src/main.cpp
@@ -3,9 +3,21 @@
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
+// Adafruit 
 Adafruit_ADS1115 ads;
 float Voltage = 0.0;
 float Distance; 
+
+// Moteur du vérin sur le L298N
+int motor1Pin1 = 17; 
+int motor1Pin2 = 16; 
+int enable1Pin = 18; 
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
 
 void setup(void) 
 {
@@ -26,6 +38,14 @@ GAIN_SIXTEEN	0.256V	±0.256V	Tension = (adc × 0.256) / 32767
 */
 
 Serial.println("ADS1115 initialise avec GAIN_ONE (±4.096V)");
+ pinMode(motor1Pin1, OUTPUT);
+ pinMode(motor1Pin2, OUTPUT);
+
+  // configure LEDC PWM (channel 0 on pin 18)
+  ledcSetup(pwmChannel, freq, resolution);
+  ledcAttachPin(enable1Pin, pwmChannel);
+
+  Serial.print("Testing DC Motor...");
 }
 
 void loop(void) 
@@ -36,28 +56,46 @@ numCapteur = ads.readADC_SingleEnded(0); // Lire la valeur du canal en mode comm
 
 Voltage = (numCapteur* 4.096) / 32767; // formule à modifier en fonction du gain choisi
 
-Serial.println("------------------------");
 Serial.print("AIN0: "); 
-//Serial.println(numCapteur);
+Serial.println(numCapteur);
 Serial.print("Voltage: ");
 Serial.println(Voltage, 7); 
-  
-Serial.println("------------------------");
+
 Serial.println("Valeur numérique :");
 Serial.println(numCapteur);
 Serial.println("Distance (cm) :");  
 // Équation du système : 62,9 + -0,0145x + 1,65E-06x^2 + -9,32E-11x^3 + 2,05E-15x^4
-Distance = 62.9
+  Distance = 62.9
                     - 0.0145 * numCapteur
                     + 1.65e-06 * pow(numCapteur, 2)
                     - 9.32e-11 * pow(numCapteur, 3)
                     + 2.05e-15 * pow(numCapteur, 4);
 
 Serial.println(Distance);
-Serial.println("------------------------");
+Serial.println("---");
 
-delay(1000);
+  // consigne 
+  float objectif = 0.0; 
+   
+  if (Distance < objectif - 0.5) {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    ledcWrite(pwmChannel, 255);
+  }
+  else if (Distance > objectif + 0.5) {
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    ledcWrite(pwmChannel, 255);
+  }
+  else {
+    ledcWrite(pwmChannel, 0);
+  }
 
+  delay(50);
 }
+
+
+
+
 
 
